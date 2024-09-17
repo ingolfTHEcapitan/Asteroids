5dfb7da0cf64af17197fcffdab429666364ebc58
Ingolf <vitaly.litov4encko2017@gmail.com>
2024-09-17 23:55:31 +1000

    Начал реализацию вывода жизней в виде сердец

---
+++ Assets/Scripts/Player/HealthVisual.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+namespace Asteroids
+{
+	public class HealthVisual : MonoBehaviour
+	{
+		[SerializeField] private Image[] _hearts;
+		[SerializeField] private Sprite _emptyHeart;
+		[SerializeField] private Sprite _fullHeart;
+		
+		
+		private void Awake() 
+		{
+			 
+		}
+		
+		
+		private void Start()
+		{
+		
+		}
+
+		private void Update()
+		{
+		
+		}
+	}
+}
